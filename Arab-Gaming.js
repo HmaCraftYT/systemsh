1fc4accf269705b119a14941fe3e4a7f214f3e37
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-16 20:06:08 +0200

    Update Arab-Gaming.js

--- Arab-Gaming.js
+++ Arab-Gaming.js
@@ -412,23 +412,6 @@ var prefix = "sh";
  message.delete(); 
 };     
 }); 
-client.on('ready', function(){
-    var ms = 10000 ;
-    var setGame = [' +help ',' +inv','+bc','+clear','+date'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/D.JPEI`);
-    }, ms);
-
-});
 client.on('message', message => {
   if (message.content.startsWith(prefix + "id")) {
   if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
